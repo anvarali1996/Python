904fccbc94ceac93a4823a2188d3afefd8f04f61
Anvarjon <anvarjonsheraliev@gmail.com>
2024-11-28 09:06:29 +0200

    Variables

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+
+# String
 newone = 'hello'
 newtwo = 'привет!'
-print(f"salom bu inglizchada {newone} ruschada bulsa {newtwo}")
+
+# Integers
+quantity = 4
+students = 24
+
+# Float
+price = 10.99
+temperature = 36.7
+
+# Boolean
+is_student = True
+is_worker= False
+
+if is_student:
+    print("If statement (IS_STUDENT) is Working")
+else:
+    print("Python is so easy!!!(STUDNET)")
+
+print('----------------------------')
+
+if is_worker:
+    print("If statement (IS_WORKER) is Working")
+else:
+    print("Python is so easy!!! (WORKER)")
+# print(f"salom bu inglizchada {newone} ruschada bulsa {newtwo} You know '{quantity} ' languages")
+# print(f"{is_worker}")
