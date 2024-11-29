16cd1609ba292fed2360f4ab1e88422b38173550
Anvarjon <anvarjonsheraliev@gmail.com>
2024-11-29 11:55:43 +0200

    If statements

--- main.py
+++ main.py
@@ -1,68 +1,24 @@
+# age = int(input("Enter your age: "))
 #
-# adjective1 = input("enter an adjective (description)")
-# noun1 = input("enter a noun (person, place or thing")
-# adjective2 = input("enter an adjective (description)")
-# verb1 = input("enter a verb ending 'ing' ")
-# adjective3 = input("enter an adjective (description)")
+# if age >= 100:
+#     print("You're too old to sign up")
+# elif age >= 18:
+#     print("You're now signed up!")
+# elif age <= 0:
+#     print("You haven't been born yet!")
+# else:
+#     print("You must be at least 18 to sign up!!!")
 #
-#
-# print(f"Today I went to a {adjective1} zoo.")
-# print(f"In an exhibit, I saw a {noun1}")
-# print(f"{noun1} was {adjective2} and {verb1}")
-# print(f"I was  {adjective3}")
+response = input("Would you like food? (Y/N): ")
 
-# friends = 10
-# friends = friends / 2
-# friends -=2
-# friends *=4
-# ramainder = friends + 5
-# print(ramainder)
+if response == "Yes" or response == 'yes':
+    print("Have some food")
+else:
+    print("No food for you:)")
 
+# name = input("what's your name?")
+# if name == "":
+#     print("You did not write your name")
+# else:
+#     print(f"Nice to meet you {name}")
 
-# x = 3.14
-# y = 4
-# z = 5
-
-# result = round(x)
-# result = abs(y)
-# result = pow(5, 3)
-# result = max(x, y, z)
-# result = min(x, y, z)
-
-# print(result)
-
-# import math
-
-# x = 25.4
-# print(math.pi)
-# print(math.e)
-# result = math.sqrt(x)
-# result = math.ceil(x)
-# result = math.floor(x)
-
-# print(result)
-
-# import math
-#
-# radius = float(input("Enter the radius of circle"))
-#
-# circumference = 2 * math.pi * radius
-#
-# print(f"The circumference is: {round(circumference, 4)}cm")
-
-# import math
-#
-# radius = float(input("Enter the radius of circle"))
-#
-# area = math.pi * pow(radius, 2)
-#
-# print(f"The area is: {round(area, 4)}cm²")
-
-import math
-
-a = float(input("Enter side A: "))
-b = float(input("Enter side B: "))
-
-c = math.sqrt(pow(a, 2) + pow(b, 2))
-
-print(f"Side C = {c}")
