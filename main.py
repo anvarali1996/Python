0ce9d17f660f665b8bcdaf4ef05075c4253a4ac8
Anvarjon <anvarjonsheraliev@gmail.com>
2024-11-29 17:24:25 +0200

    Conditional Expressions

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-# Logical operators = evaluate multiple conditions (or, and, not)
-#             or = at least one condition must be True
-#             and = both conditions must be True
-#             not = inverts the condition (not False, not True)
+# Conditional expression = A one-line shortcut for the if-else statement (ternanry operator)
+#             print or assign one of two based value based on a condition
+#             X if condition else Y
 
-# temp = 34
-# is_raining = False
-#
-# if temp >= 35 or temp <= 0 or is_raining:
-#     print("The outdoor event is cancelled!!!")
-# else:
-#     print("The outdoor event is still scheduled")
+num = 120
+a = 5
+b = 7
+age = 16
+temperature = 21
+user_role = "guest"
 
-temp = 20
-is_sunny = False
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
 
-if temp >= 35 and is_sunny:
-    print("It is HOT outside 🥵")
-    print("It's SUNNY ☀️")
-elif temp <= 5 and is_sunny:
-    print("It's COLD outside")
-    print("It's SUNNY☀️")
-elif 28 > temp > 0 and is_sunny:
-    print("It's WARM outside")
-    print("It's SUNNY☀️☀")
-elif temp >= 28 and not is_sunny:
-    print("It's HOT outside 🥵")
-    print("It's CLOUDY☁️")
-elif temp <= 5 and not is_sunny:
-    print("It's COLD outside")
-    print("It's CLOUDY☁️")
-elif 28 > temp > 0 and not is_sunny:
-    print("It's WARM outside")
-    print("It's CLOUDY☁️")
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
