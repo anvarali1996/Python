dfb71557bb3cc94b112e72417cce28f469091374
Anvarjon <anvarjonsheraliev@gmail.com>
2024-11-29 19:14:18 +0200

    String Methods and Indexing

--- main.py
+++ main.py
@@ -1,20 +1,62 @@
-# Conditional expression = A one-line shortcut for the if-else statement (ternanry operator)
-#             print or assign one of two based value based on a condition
-#             X if condition else Y
+# name = input("enter your full name: ")
+# phone_number = input("enter your phoneNumber: ")
 
-num = 120
-a = 5
-b = 7
-age = 16
-temperature = 21
-user_role = "guest"
+# result = len(name)
+# result = name.find("r")
+# result = name.rfind("r")
+# result = name.capitalize()
+# result = name.upper()
+# result = name.lower()
+# result = name.isalpha()
+# result = name.isdigit()
+# result = phone_number.count("-")
+# phone_number = phone_number.replace("-", "")
 
-# result = "EVEN" if num % 2 == 0 else "ODD"
-# max_num = a if a > b else b
-# min_num = a if a < b else b
 
-# status = "Adult" if age >= 18 else "Child"
-# weather = "HOT" if temperature > 20 else "COLD"
-access_level = "Full Access" if user_role == "admin" else "Limited Access"
 
-print(access_level)
+# ism = "Anvarjon"
+# result = name.replace(name, ism)
+# print(f"Do not lie!!! Your name is {ism}")
+
+
+
+# print(phone_number)
+
+# print(help(str))
+
+# =========------TASK-----=========
+
+# username = input("Enter a username:")
+#
+# if len(username) >= 12:
+#     print("There 12 characters")
+# #     BELOW IS also true. It works!!!
+# # elif not username.find(" ") == -1:
+# elif " " in username:
+#     print("There is space")
+# elif not username.isalpha():
+#     print("There're some digits")
+# else:
+#     print(f"You have successfully regisreted!!! WELCOME {username}")
+
+
+# ===============-------------- STRING INDEXING -------------==================
+
+# indexing  = accessing elements of a sequence using [] (indexing operators)
+#  [start: end: step]
+
+credit_number = '1234-5678-9012-3456'
+
+# print(credit_number[0])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-1])
+# print(credit_number[::2])
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# it shows on reverse form!!!
+last_digits = credit_number[::-1]
+print(last_digits)
