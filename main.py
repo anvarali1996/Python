8d14f0adc864d9ddee70c1fd1d77f76a458d4b51
Anvarjon <anvarjonsheraliev@gmail.com>
2024-11-30 13:46:11 +0200

    Python Calculator

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
-# ## ==========/////////--- IT'S CALCULATOR ----/////////===============
+# Python calculator
 
-principle = 0
-rate = 0
-time = 0
+operator = input("Enter operator (+ - * /): ")
 
-while True:
-    # while principle <= 0:
-    principle =float(input("Enter the principle amount: "))
-    if principle < 0:
-        print("Principle can't be less than or equal to zero!")
-    else:
-        break
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
-# while rate <= 0:
-while True:
-    rate = float(input("Enter the interest rate: "))
-    if rate < 0:
-        print("Interest rate can't be less than or equal to zero!")
-    else:
-        break
-# while time <= 0:
-while True:
-    time = float(input("Enter the time in years: "))
-    if time < 0:
-        print("Time can't be less than or equal to zero!")
-    else:
-        break
-
-total = principle * pow((1 + rate / 100), time)
-print(f"Balance after {time} year/s: ${total:.2f}")
-
-
-# for i in range(1, 11):
-#     print(i)
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2))
+    # print(num1 + num2)
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+    # print(num1 - num2)
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+    # print(num1 * num2)
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
+    # print(num1 / num2)
+else:
+    print("Please write these 4 emblems")
