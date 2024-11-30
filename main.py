cd2d9a7e8ecab9e61dc5d71df0cdb135ca97bbb9
Anvarjon <anvarjonsheraliev@gmail.com>
2024-11-30 20:48:59 +0200

    Lists, Sets and Tuple

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-# ## ==========/////////--- IT'S CALCULATOR ----/////////===============
+# Lists, Sets, Tuples
 
-principle = 0
-rate = 0
-time = 0
+# fruits = ["apple", "orange", "banana", "coconut"]
+# print(dir(fruits))
+# print(len(fruits))
+# print(fruits[0::])
 
-while True:
-    # while principle <= 0:
-    principle =float(input("Enter the principle amount: "))
-    if principle < 0:
-        print("Principle can't be less than or equal to zero!")
-    else:
-        break
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(1, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
 
-# while rate <= 0:
-while True:
-    rate = float(input("Enter the interest rate: "))
-    if rate < 0:
-        print("Interest rate can't be less than or equal to zero!")
-    else:
-        break
-# while time <= 0:
-while True:
-    time = float(input("Enter the time in years: "))
-    if time < 0:
-        print("Time can't be less than or equal to zero!")
-    else:
-        break
-
-total = principle * pow((1 + rate / 100), time)
-print(f"Balance after {time} year/s: ${total:.2f}")
+# for fruit in fruits:
+#     print(fruit)
 
 
-# for i in range(1, 11):
-#     print(i)
+# ======////-------- SETS ------////=========
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# fruits.add("Pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+# fruits.clear()
+
+
+# print(fruits)
+
+# ==========////////////---------- TUPLE -------////////////============
+
+fruits = ("apple", "orange", "banana", "coconut", "apple")
+
+# print(fruits.index("orange"))
+# print(fruits.count("apple"))
+
+# print(fruits)
+
+for fruit in fruits:
+    print(fruit)
