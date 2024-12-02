a82cc1422753ddf99d36e46d28f2f7ed158e2509
Anvarjon <anvarjonsheraliev@gmail.com>
2024-12-02 19:19:57 +0200

    Random Number

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-# ## ==========/////////--- IT'S CALCULATOR ----/////////===============
+# import random
+#
+# low = 1
+# high = 100
+# options = ("rock", "scissors", "paper")
+# cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+#
+# # number = random.randint(low, high)
+# # number = random.random()
+# # options = random.choice(options)
+# random.shuffle(cards)
+#
+# # print(f"{number:.2f}")
+# print(cards)
 
-principle = 0
-rate = 0
-time = 0
 
-while True"":
-    # while principle <= 0:
-    principle =float(input("Enter the principle amount: "))
-    if principle < 0:
-        print("Principle can't be less than or equal to zero!")
+# PYTHON number guessing game
+
+import random
+
+lowest_num = 1
+highest_num = 10
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
+
+print("Python Number Guessing Game")
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+    guess = input("enter your guess: ")
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"Please select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low! Try again")
+        elif guess > answer:
+            print("Too high! Try again!")
+        else:
+            print(f"CORRECT! The answer was {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running
     else:
-        break
-
-# while rate <= 0:
-while True:
-    rate = float(input("Enter the interest rate: "))
-    if rate < 0:
-        print("Interest rate can't be less than or equal to zero!")
-    else:
-        break
-# while time <= 0:
-while True:
-    time = float(input("Enter the time in years: "))
-    if time < 0:
-        print("Time can't be less than or equal to zero!")
-    else:
-        break
-
-total = principle * pow((1 + rate / 100), time)
-print(f"Balance after {time} year/s: ${total:.2f}")
-
-
-# for i in range(1, 11):
-#     print(i)
+        print("Invalid guess")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
