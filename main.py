115c9ddc33775758a74dc982f37d4e3bb840bad3
Anvarjon <anvarjonsheraliev@gmail.com>
2024-12-03 23:33:20 +0200

    Default and Keyword arguments

--- main.py
+++ main.py
@@ -1,47 +1,34 @@
-# def happy_birthday(name, age):
-#     print(f"Happy birthday to you! {name}")
-#     print(f"You're {age} years old")
+# DEFAULT ARGUMENTS
+# def net_price(list_price, discount = 0, tax=0.05):
+#     return list_price * (1 - discount) * (1 + tax)
 #
-# happy_birthday("Anvarjon", 25)
-# happy_birthday("Nurmuhammad", 2)
-
-# def display_invoice(username, amount, due_date):
-#     print(f"Hello {username}!")
-#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
-#
-# display_invoice("anvarjon", 100.99, "01/02")
-
-# def add(x, y):
-#     z = x + y
-#     return z
-# def subtract(x, y):
-#     z = x - y
-#     return z
-# def multiply(x, y):
-#     z = x * y
-#     return z
-# def divide(x, y):
-#     z = x / y
-#     return z
-#
-#
-# def add(x, y):
-#     z = x + y
-#     return z
-#
-#
-# print(add(1, 2))
-# print(subtract(8 , 2))
-# print(multiply(5, 2))
-# print(divide(9, 2))
-#
+# # print(net_price(500))
+# # print(net_price(500, 0.1))
+# print(net_price(500, 0.1, 0))
 
 
-def create_name(first, last):
-    first = first.capitalize()
-    last = last.capitalize()
-    return first + " " + last
+# import time
+# def count(start, end):
+#     for x in range(start, end+1):
+#         print(x)
+#         time.sleep(1)
+#     print("DONE")
+#
+# count(1, 10)
 
-full_name = create_name("Anvarjon", "Sheraliev")
+# KEY ARGUMENTS
 
-print(full_name)
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title}{first} {last}")
+#
+# hello("Hello", title="Mr.", last="John", first="James")
+
+# for x in range(1, 11):
+#     print(x, end=" ")
+
+# print("1","2", "3", "4", "5", sep="-")
+
+def get_phone(country, area, first, last):
+    return f"{country}-{area}-{first}-{last}"
+phone_num = get_phone(country=998, area=93, first=299, last=4343)
+print(phone_num)
