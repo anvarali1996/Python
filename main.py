f70da8135c3039ec352842a6727315c3ea690fd3
Anvarjon <anvarjonsheraliev@gmail.com>
2024-12-05 09:24:26 +0200

    List Comprehensions

--- main.py
+++ main.py
@@ -1,64 +1,30 @@
-# Iretable
-
-# numbers = [1, 2, 4, 5, 6]
-# for number in reversed(numbers):
-#     print(number, end=" ")
-
-# name = "Anvarjon Sheraliev"
+# double = []
+# for i in range(1, 11):
+#     double.append(i * 2)
 #
-# for letter in name:
-#     print(letter.upper(), end="")
+# print(double)
 
-# my_test = {"A": 1, 'B': 2, "C": 3}
+# numbers = [num for num in range(1, 11)]
+# double = [x * 2 for x in range(1, 11)]
+# triple = [x * 3 for x in range(1, 11)]
+# square = [x * x for x in range(1, 11)]
 #
-# for key, value in my_test.items():
-#     # print(f"{key}: {value}")
-#     print(key, value)
+# print(numbers)
+# print(double)
+# print(triple)
+# print(square)
 
+# fruits = ["apple", "orange", "strawberry", "pear"]
+# fruits = [i.upper() for i in fruits]
+# print(fruits)
 
-# //////=====----- MEMBERSHIP OPERATORS -----======//////
+numbers = [1, -2, 3, -4, 5, -6, 7, 9, 8, 87, 98]
+positive_num = [num for num in numbers if num >= 0]
+negative_num = [num for num in numbers if num <= 0]
+even_num = [num for num in numbers if num % 2 == 0]
+odd_num = [num for num in numbers if num % 2 != 0]
 
-# word = "APPLE"
-#
-# letter = input("Guess a letter in the secret word: ")
-#
-# # if letter in word:
-# #     print(f"There is a {letter}")
-# # else:
-# #     print(f"{letter} was not found")
-#
-# if letter not in word:
-#     # print(f"There is a {letter}")
-#     print(f"{letter} was NOT found")
-#
-# else:
-#     print(f"{letter} was found")
-
-# students = {"Ali", "Bob", "Someone"}
-#
-# student = input("enter a student name: ")
-#
-# if student in students:
-#     print(f"{student} is student")
-# else:
-#     print(f"{student} was not found!!!")
-
-# grades = {"Ali": 1, "Bob": "5-score", "Someone": "8 score"}
-# student = input("Enter the name of student: ")
-#
-# # if student in grades:
-# #     print(f"{student}'s grade is {grades[student]}")
-# # else:
-# #     print(f"{student} was not found")
-#
-# for key, value in grades.items():
-#     print(student)
-#     print(f"{key}: {value}")
-
-
-email = "info@anvarjon.eu"
-
-if "@" in email and "." in email:
-    print("Valid email")
-else:
-    print("Invalid email")
+print(positive_num)
+print(f"Negative numbers: {negative_num}")
+print(even_num)
+print(odd_num)
