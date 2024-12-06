8f6780c2e1b926a6aef226cbc86773c98a7ac335
Anvarjon <anvarjonsheraliev@gmail.com>
2024-12-06 13:54:04 +0200

    Modules

--- main.py
+++ main.py
@@ -1,61 +1,10 @@
-# MATCH CASE STATEMENTS COMPREHENSION
-
-# today = int(input("Enter today: "))
-
-
-# def day_of_week(day):
-#     # today = input("Enter today: ")
-#     match day:
-#         case 1:
-#             return "It's Monday"
-#         case 2:
-#             return "It's Tuesday"
-#         case 3:
-#             return "It's Wednesday"
-#         case 4:
-#             return "It's Thursday"
-#         case 5:
-#             return "It's Friday"
-#         case 6:
-#             return "It's Saturday"
-#         case 7:
-#             return "It's Sunday"
-#         case _:
-#             return "Not a valid day!!!"
+# import example
 #
-# print(day_of_week(today))
-
-
-# today = input("Enter today: ")
-# def day_of_week(day):
-#     # today = input("Enter today: ")
-#     match day:
-#         case "Monday":
-#             return "It's Monday"
-#         case "Tuesday":
-#             return "It's Tuesday"
-#         case "Wednesday":
-#             return "It's Wednesday"
-#         case "Thursday":
-#             return "It's Thursday"
-#         case "Friday":
-#             return "It's Friday"
-#         case "Saturday":
-#             return "It's Saturday"
-#         case "Sunday":
-#             return "It's Sunday"
-#         case _:
-#             return "Not a valid day!!!"
+# result = example.pi
+# result = example.square(2)
+# result = example.cube(3)
+# result = example.circumference(3)
 #
-# print(day_of_week(today))
-
-# def is_weekend(day):
-#     match day:
-#         case  "Saturday" | "Sunday":
-#             return  True
-#         case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
-#             return False
-#         case _:
-#             return False
 #
-# print(is_weekend ("Sunday"))
+#
+# print(result)
